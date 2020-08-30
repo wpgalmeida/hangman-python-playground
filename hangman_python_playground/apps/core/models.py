d445f1dd32de91ed6a8adb637178382230f8ffd9
Wellington Almeida <wpgalmeida@gmail.com>
2020-08-30 13:31:15 -0300

    Criação das tabelas (Models)

--- hangman_python_playground/apps/core/models.py
+++ hangman_python_playground/apps/core/models.py
@@ -1,3 +1,40 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+class StandardModelMixin(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-created_at", "-updated_at"]
+
+
+class Player(StandardModelMixin):
+    name = models.CharField(max_length=100, blank=False)
+    birth = models.DateField()
+    gender = models.CharField(max_length=1, blank=False)  # Male, Female, Others
+
+
+class Categories(StandardModelMixin):
+    name_category = models.CharField(max_length=20, blank=False)
+
+
+class Words(StandardModelMixin):
+    word = models.CharField(max_length=20, blank=False)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+
+
+class Move(StandardModelMixin):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    letter = models.CharField(max_length=1, blank=False)
+    right = models.BooleanField(default=False)
+
+
+class Game(StandardModelMixin):
+    word = models.ForeignKey(Words, on_delete=models.CASCADE)
+    move = models.ForeignKey(Move, on_delete=models.CASCADE, null=True)
+    end_game = models.BooleanField(default=False)
